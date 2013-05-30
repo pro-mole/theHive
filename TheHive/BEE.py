32cef637be36cf8a5b3193d96fccb267f562f551
Bruno Guedes <professional.mole@yahoo.com.br>
2013-05-29 22:53:49 -0300

    Starting to use enums

--- TheHive/BEE.py
+++ TheHive/BEE.py
@@ -1,6 +1,11 @@
 '''BEE module
 Codifies BEEs, our main game agent'''
 
+'''Enumerations'''
+enumBEEFunction = ["WORKER", "QUEEN", "SOLDIER"]
+enumBEESkill = ["HONEYMAKE","HIVEBUILD", "EGGLAY", "EGGTEND", "QUEENTEND", "FOODFIND", "SCOUT"]
+enumBEEState = ["IDLE","SEARCHING","WARNING","WORKING","RESTING"]
+
 class BEE:
     
     '''Position'''
@@ -13,16 +18,24 @@ class BEE:
     hunger = 0
     '''Energy'''
     energy = 1000
-    
+
+    '''AI parameters'''
+    '''Function: what does this BEE do?'''
+    function = "WORKER"
     '''State: what is this BEE doing now?'''
     state = "IDLE"
 
     '''Create a BEE at position (H,I,J), associated with world W'''
     def __init__(self,h,i,j,W):
-        pos = (h,i,j)
-        world = W
+        self.pos = (h,i,j)
+        self.world = W
     
     def __repr__(self):
         R = "BEE {0} at ({1[0]},{1[1]},{1[2]})\n".format(id(self), self.pos)
         R += "HUNGER: {0}   ENERGY: {1}".format(self.hunger, self.energy)
         return R
+    
+    def setFunction(self, function):
+        if enumBEEFunction.count(function) > 0:
+            self.function = function
+        
